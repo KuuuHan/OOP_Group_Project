8cbc058f59d758173a9737f39aadf970631cc7fe
Kuu <s3986602@rmit.edu.vn>
2024-05-16 00:13:31 +0700

    fix error on logout of Dependents.fxml

--- src/main/java/ui/gp/SceneController/SceneUtil.java
+++ src/main/java/ui/gp/SceneController/SceneUtil.java
@@ -1,10 +1,15 @@
 package ui.gp.SceneController;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.Objects;
@@ -12,26 +17,31 @@ import java.util.Optional;
 
 public class SceneUtil {
     private static AnchorPane currentScene;
-    public static void setCurrentScene(AnchorPane scene) {
-        currentScene = scene;
+
+    public static void setCurrentScene(AnchorPane currentScene) {
+        SceneUtil.currentScene = currentScene;
     }
+
     public static void logout(AnchorPane pane) throws IOException {
-        Optional<ButtonType> comfirmation = comfirmationDialog(
+        Optional<ButtonType> confirmation = confirmationDialog(
                 "Logout",
                 "Are you sure you want to logout?",
                 "You will be redirected to the home page"
         );
 
-        if (comfirmation.get() == ButtonType.OK) {
+        if (confirmation.get() == ButtonType.OK) {
+            setCurrentScene(pane);
             changeScene (pane, "/ui/gp/Scene/Home.fxml");
+
             // luu lai thong tin
         }
     }
 
-    private static void changeScene(AnchorPane pane, String fxmlPath) throws IOException {
+    private static void changeScene(AnchorPane currentScene, String fxmlPath) throws IOException {
         AnchorPane nextScene = FXMLLoader.load(Objects.requireNonNull(SceneUtil.class.getResource(fxmlPath)));
-        changeCleanScene(pane, nextScene);
+        changeCleanScene(currentScene, nextScene);
     }
+
 
     private static void changeCleanScene(AnchorPane currentScene, AnchorPane nextScene) {
         currentScene.getChildren().removeAll();
@@ -39,11 +49,12 @@ public class SceneUtil {
     }
 
 
-    private static Optional<ButtonType> comfirmationDialog(String title, String header, String content) {
+    private static Optional<ButtonType> confirmationDialog(String title, String header, String content) {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.setTitle(title);
         alert.setHeaderText(header);
         alert.setContentText(content);
         return alert.showAndWait();
     }
+
 }
