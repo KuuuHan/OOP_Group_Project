5ac43cfb16e9f0f7b4a14bf7bf3aa9d85d635bb7
Han Khang <s3986602@rmit.edu.vn>
2024-05-15 14:40:40 +0700

    1st create

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module ui.gp {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens ui.gp to javafx.fxml;
+    exports ui.gp;
+}
