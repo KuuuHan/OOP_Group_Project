bb8b938788867ee519e4c49279e5fc6dd83cedbe
spjk1910 <trangdpt1910@gmail.com>
2024-05-16 20:39:49 +0700

    add Model.java amd ViewFactory.java

---
+++ src/main/java/ui/gp/Models/Model.java
@@ -0,0 +1,127 @@
+package ui.gp.Models;
+
+import ui.gp.Models.Users.*;
+import ui.gp.Utils.*;
+import ui.gp.View.*;
+
+import java.sql.ResultSet;
+
+public class Model
+{
+    private static Model model;
+    private final ViewFactory view;
+    private final DatabaseConnection databaseConnection;
+    private Role loginRole;
+    private Dependent dependent;
+    private SystemAdmin admin;
+    private PolicyOwner policyOwner;
+    private PolicyHolder policyHolder;
+    private InsuranceManager insuranceManager;
+    private InsuranceSurveyor insuranceSurveyor;
+    private boolean loginSuccess;
+
+    public Model()
+    {
+        this.view = new ViewFactory();
+        this.databaseConnection = new DatabaseConnection();
+        this.dependent = new Dependent("","","",Role.Dependent,"","","","",null,null,null,null);
+        this.admin = new SystemAdmin("","","",Role.System_Admin,"","","","");
+        this.policyOwner = new PolicyOwner("","","",Role.Policy_Owner,"","","","",null);
+        this.policyHolder = new PolicyHolder("","","",Role.Policy_Holder,"","","","",null,null,null,null);
+        this.insuranceManager = new InsuranceManager("","","",Role.Insurance_Manager,"","","","");
+        this.insuranceSurveyor = new InsuranceSurveyor("","","",Role.Insurance_Surveyor,"","","","");
+        this.loginSuccess = false;
+    }
+
+    public static synchronized Model getInstance()
+    {
+        if (model == null)
+        {
+            model = new Model();
+        }
+        return model;
+    }
+
+    public ViewFactory getView()
+    {
+        return view;
+    }
+
+    public DatabaseConnection getDatabaseConnection()
+    {
+        return databaseConnection;
+    }
+
+    public Role getLoginRole()
+    {
+        return loginRole;
+    }
+
+    public boolean getLoginSuccess()
+    {
+        return loginSuccess;
+    }
+
+    public void setLoginRole(Role loginRole)
+    {
+        this.loginRole = loginRole;
+    }
+
+    public void evaluateUserCred(String username, String password) {
+        Role role = databaseConnection.getUserRole(username);
+        ResultSet resultSet = null;
+        if (role != null) {
+            switch (role) {
+                case Dependent:
+                    resultSet = databaseConnection.getDependentData(username, password);
+                    break;
+                case System_Admin:
+                    resultSet = databaseConnection.getAdminData(username, password);
+                    break;
+                case Policy_Owner:
+                    resultSet = databaseConnection.getPolicyOwnerData(username, password);
+                    break;
+                case Policy_Holder:
+                    resultSet = databaseConnection.getPolicyHolderData(username, password);
+                    break;
+                case Insurance_Manager:
+                    resultSet = databaseConnection.getInsuranceManagerData(username, password);
+                    break;
+                case Insurance_Surveyor:
+                    resultSet = databaseConnection.getInsuranceSurveyorData(username, password);
+                    break;
+            }
+
+            if (resultSet != null) {
+                try {
+                    if (resultSet.isBeforeFirst()) {
+                        this.loginRole = role;
+                        this.loginSuccess = true;
+                        switch (role) {
+                            case Dependent:
+                                //Load Dependent data
+                                break;
+                            case System_Admin:
+                                //Load Admin data
+                                break;
+                            case Insurance_Surveyor:
+                                //Load Surveyor data
+                                break;
+                            case Insurance_Manager:
+                                //Load Manager data
+                                break;
+                            case Policy_Holder:
+                                //Load Policy Holder data
+                                break;
+                            case Policy_Owner:
+                                //Load Policy Owner data
+                                break;
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/ui/gp/View/ViewFactory.java
@@ -0,0 +1,4 @@
+package ui.gp.View;
+
+public class ViewFactory {
+}
