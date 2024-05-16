67288294945cae951c8a36bdb922d65af235fd28
Kuu <s3986602@rmit.edu.vn>
2024-05-16 22:48:09 +0700

    Turn off initialize() to make the code work

--- src/main/java/ui/gp/SceneController/Customer/DependentsHomeController.java
+++ src/main/java/ui/gp/SceneController/Customer/DependentsHomeController.java
@@ -2,10 +2,12 @@ package ui.gp.SceneController.Customer;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.*;
+import ui.gp.SceneController.Customer.Dependents.*;
 import ui.gp.SceneController.SceneUtil;
-
 import java.io.IOException;
 
 public class DependentsHomeController {
@@ -13,6 +15,11 @@ public class DependentsHomeController {
     Label welcomeBannerUser;
     @FXML
     AnchorPane dependentsHomeScene;
+    @FXML
+    TabPane dependentsTabPane;
+    private ClaimViewController claimViewController;
+    private InformationViewController informationViewController;
+
 
     public void bannerNameView(String username) {
         welcomeBannerUser.setText("Welcome " + username);
@@ -22,5 +29,21 @@ public class DependentsHomeController {
         System.out.println("Dependents logout");
         SceneUtil.logout(dependentsHomeScene);
     }
+
+//    public void initialize(){
+//        try{
+//            FXMLLoader fxmlLoader = new FXMLLoader();
+//            fxmlLoader.setLocation(getClass().getResource("/ui/gp/Scene/Customer/Dependents/claim.fxml"));
+//
+//            Tab claimTab = new Tab("Claim View");
+//            claimTab.setContent(fxmlLoader.load());
+//            claimViewController = fxmlLoader.getController();
+//
+//            dependentsTabPane.getTabs().add(claimTab);
+//        } catch (IOException e){
+//            System.out.println(e);
+//        }
+//
+//    }
 }
 
